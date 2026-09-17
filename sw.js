9fc22e80671a899fd3cd9df08633744e6d335f86
vhuang904 <vhuang904@gmail.com>
2026-09-17 09:41:44 +0800

    Update cache version to ride2gether-cache-v7.6

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // RIDE2GETHER Service Worker
 // ==========================================
 
-const CACHE_NAME = 'ride2gether-cache-v7.5';
+const CACHE_NAME = 'ride2gether-cache-v7.6';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
